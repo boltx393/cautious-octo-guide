e79184baeaba7ea284bf1eff18fe0af0b4bfddf6
Aayush Chhikara <88643777+boltx393@users.noreply.github.com>
2025-01-02 17:18:44 +0530

    modified bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 # Dictionary to track daily message counts per user
 message_count = {}
